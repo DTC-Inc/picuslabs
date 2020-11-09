48dd9b23551f66463e7697398d0e66c8141a2eff
Nate Smith <40123869+nettts@users.noreply.github.com>
2020-11-09 11:17:20 -0500

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -2,5 +2,6 @@
 $dcname=[System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain().PdcRoleOwner.Name.split('.')[0]
 $dcip=[System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain().PdcRoleOwner.IPAddress
 echo "[*] DC: $dcip ($dcname)"
-$env:windir\temp\zerologon-tester-master\zerologon_tester.exe $dcname $dcip
+cd $env:windir\temp\zerologon-tester-master\bin
+.\zerologon_tester.exe $dcname $dcip
 pause
